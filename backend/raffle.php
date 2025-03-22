11d43cf59fe086758379367583d4562870238283
degrandelucas <lucasdegrande15@gmail.com>
2025-03-21 22:54:44 -0300

    starting database connection for raffle.php file, using trait

--- backend/raffle.php
+++ backend/raffle.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace Backend;
+
+require '../vendor/autoload.php';
+require '../traits/DatabaseConnection.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Traits\DatabaseConnection;
+
+class Raffle{
+    use DatabaseConnection;
+
+    public function drawParticipant()
+    {
+        try {
+            $connection = $this->getConnection();
+            $sql = "SELECT * FROMN participants ORDER BY RAND() LIMIT 1";
+            $result = $connection->query($sql);
+
+            if ($result->num_rows > 0) {
+                $participant = $result->fetch_assoc();
+                $connection->close();
+                return $participant;
+            } else {
+                $connection->close();
+                return ['error' => 'Nenhum participante encontrado.'];
+            }
+        } catch (\Exception $error) {
+            return ['error' => 'Erro: ' . $error->getMessage()];
+        }
+    }
+}
